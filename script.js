8dbc4f4f747e6d0f68813643f399e48d72d2edc2
gaurav kumar <gauravsaroj7800@gmail.com>
2023-11-02 19:37:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "C++ programmer", "Data Structure enthusiast","Python programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    backDelay: 1000,
+    loop: true
+});
